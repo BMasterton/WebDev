ca95e1e9324b44ce1ec1b46fc679cf57a02d7325
Braonain <braonainmasterton@gmail.com>
2024-02-06 10:08:44 -0800

    added global info array for api

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -1,3 +1,13 @@
+const messages = [
+    { id: 0, name: 'Bill', msgText: 'Hi All!' },
+    { id: 1, name: 'Ann', msgText: 'ICS 211 is fun!' },
+    { id: 2, name: 'Johnny', msgText: `I'm stranded!` },
+    { id: 3, name: 'Barb', msgText: 'Hi' },
+    { id: 4, name: 'Frank', msgText: `Who's tired?` },
+    { id: 5, name: 'Sarah', msgText: 'I Heart React' },
+];
+
+
 // GET Request Handler
 const getAllMessages = (req, res) => {
     res.status(200).send('Successful API GET Request');
