b660309660f80acedd6fd7a0ffacc8e14c8f2bee
Braonain <braonainmasterton@gmail.com>
2024-03-19 14:05:19 -0700

    Added basicStrategy to webservice

--- controllers/user-api-controller.js
+++ controllers/user-api-controller.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import passport from 'passport';
+import { BasicStrategy } from 'passport-http';
+
 
 const userModel = mongoose.model('user');
 
@@ -27,5 +30,31 @@ try {
         res.status(400).send('Bad Request. The message in the body of the \ Request is either missing or malformed.');
         }
     }
+
+
+    passport.use(new BasicStrategy(
+        async (userIdent, password, done) => {
+            try {
+                const user = await userModel.findOne({
+                    '$or': [
+                        { email: userIdent },
+                        { username: userIdent }
+                    ]
+                }).exec();
+            // user wasn't found
+                if (!user) return done(null, false);
+                // user was found, see if it's a valid password
+                if (!await user.verifyPassword(password)) {
+                // password not valid
+                    return done(null, false);
+                }
+                // valid password, return user
+                    return done(null, user);
+                } catch (error) {
+                // error searching for user
+                    return done(error);
+                }
+            }
+        ));
 
 export {registerNewUser};
