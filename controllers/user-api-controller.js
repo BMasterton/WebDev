079228064161bac5b54651a6b9a14240be42e43e
Braonain <braonainmasterton@gmail.com>
2024-03-19 13:54:44 -0700

    implemented registerNewUser handler for the /users endpoit

--- controllers/user-api-controller.js
+++ controllers/user-api-controller.js
@@ -3,8 +3,29 @@ import mongoose from 'mongoose';
 const userModel = mongoose.model('user');
 
 
+const alreadyExists = async ( email, username ) => (
+    await userModel.exists({
+    '$or': [
+    { email: email },
+    { username: username }
+    ]
+    })
+    );
+
 const registerNewUser = async (req, res) => {
-    res.status(200).send('Successful API New User POST Request');
+
+try {
+    if( await alreadyExists(req.body.email, req.body.username)){
+        res.status(423).send("Forbidden - Username or Email already exists")
+    } 
+    else {
+        let user = await userModel.create(req.body);
+        res.status(201).json(user);
+    }
+
+        } catch (err) {
+        res.status(400).send('Bad Request. The message in the body of the \ Request is either missing or malformed.');
+        }
     }
 
 export {registerNewUser};
