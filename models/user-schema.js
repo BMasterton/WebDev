67f1ba7b32d636c8f97ad9543280d900e96e3c18
Braonain <braonainmasterton@gmail.com>
2024-03-19 13:58:49 -0700

    Added argon 2 password verification to the userschema

--- models/user-schema.js
+++ models/user-schema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import argon2 from 'argon2';
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -31,6 +32,17 @@ userSchema.set('toJSON', {
     transform: (doc, ret) => { delete ret._id; }
     });
 
-
+// Pre-Hook to Salt and Hash a password using argon2id
+userSchema.pre('save', async function() {
+    // hash and salt password
+    try {
+        const hash = await argon2.hash(this.password, {
+        type: argon2.argon2id
+    });
+        this.password = hash;
+    } catch (err) {
+        console.log('Error in hashing password' + err);
+    }
+});
 
 export default mongoose.model('user', userSchema);
