720fa6f1264f2d67d38248ce79dce27032f7cd19
Braonain <braonainmasterton@gmail.com>
2024-04-10 14:59:17 -0700

    Added delete api web service call

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllMessages, addNewMessage, updateMessage } from '../controllers/msg-api-controller.js';
+import { getAllMessages, addNewMessage, updateMessage, deleteMessage } from '../controllers/msg-api-controller.js';
 import { registerNewUser, logInUser } from '../controllers/user-api-controller.js';
 import passport from 'passport';
 
@@ -18,5 +18,8 @@ router.route('/login')
 
 router.route('/messages/:messageId')
 .patch(passport.authenticate('jwt', {session: false}), updateMessage);
+
+router.route('/messages/:messageId')
+.delete(passport.authenticate('jwt', {session: false}), deleteMessage);
 
 export default router;
